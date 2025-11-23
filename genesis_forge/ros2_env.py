8de44378572ecc8043c816a1c677127a36464e25
vybhav@gmail.com <ibr.vybhav@gmail.com>
2025-11-23 12:51:22 +0530

    add a ros2 manager env for direct deployment to ros

---
+++ genesis_forge/ros2_env.py
@@ -0,0 +1,292 @@
+from __future__ import annotations
+import math
+import torch
+from gymnasium import spaces
+from typing import Any, Literal, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from genesis.engine.entities import RigidEntity
+
+EnvMode = Literal["train", "eval", "play"]
+
+
+class Ros2Env:
+    """
+    Base environment class for your simulated robot environment.
+
+    Args:
+        num_envs: Number of parallel environments.
+        dt: Simulation time step.
+        max_episode_length_sec: Maximum episode length in seconds.
+        max_episode_random_scaling: Scale the maximum episode length by this amount (+/-) so that not all environments reset at the same time.
+        extras_logging_key: The key used, in info/extras dict, which is returned by step and reset functions, to send data to tensorboard by the RL agent.
+
+    Example::
+
+        class MyEnv(GenesisEnv):
+            def __init__(self, *args, **kwargs):
+                super().__init__(*args, **kwargs)
+
+                # ...Define scene here...
+                self.scene = gs.Scene()
+                self.terrain = self.scene.add_entity(gs.morphs.Plane())
+                self.robot = self.scene.add_entity( ... )
+
+            def step(self, actions: torch.Tensor) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, dict[str, Any]]:
+                # ...step logic here...
+                return obs, rewards, terminations, truncations, info
+
+            def reset(self, envs_idx: list[int] = None) -> tuple[torch.Tensor, dict[str, Any]]:
+                # ...reset logic here...
+                return obs, info
+
+            def get_observations(self) -> torch.Tensor:
+                # ...define current observations here...
+                return obs
+
+    """
+
+    action_space: spaces.Space | None = None
+    observation_space: spaces.Space | None = None
+    can_be_wrapped: bool = True
+
+    def __init__(
+        self,
+        num_envs: int = 1,
+        dt: float = 1 / 100,
+        max_episode_length_sec: int | None = 10,
+        max_episode_random_scaling: float = 0.0,
+        extras_logging_key: str = "episode",
+    ):
+        self.dt = dt
+        self.num_envs = num_envs
+        if torch.cuda.is_available():
+            self.device = torch.device("cuda:0") 
+        else:
+            self.device = torch.device("cpu")  # Fall back to CPU if no GPU is available
+
+        self.extras_logging_key = extras_logging_key
+        self._extras = {}
+        self._extras[extras_logging_key] = {}
+
+        self._actions: torch.Tensor = None
+        self._last_actions: torch.Tensor = None
+
+        self.step_count: int = 0
+        self.episode_length = torch.zeros(
+            (self.num_envs,), device=self.device, dtype=torch.int32
+        )
+        self.max_episode_length: torch.Tensor = None
+
+        self._max_episode_length_sec = 0.0
+        self._base_max_episode_length = None
+        self._max_episode_random_scaling = max_episode_random_scaling
+        if max_episode_length_sec and max_episode_length_sec > 0:
+            self.max_episode_length = torch.zeros(
+                (self.num_envs,), device=self.device, dtype=torch.int
+            )
+            self.max_episode_length[:] = self.set_max_episode_length(
+                max_episode_length_sec
+            )
+
+    """
+    Properties
+    """
+
+    @property
+    def unwrapped(self):
+        """Returns this environment, not a wrapped version of it."""
+        return self
+
+    @property
+    def max_episode_length_sec(self) -> int | None:
+        """The max episode length, in seconds, for each environment."""
+        return self._max_episode_length_sec
+
+    @property
+    def extras(self) -> dict:
+        """
+        The extras/infos dictionary reset at the start of every step, and contains additional data about the environment during that step.
+        """
+        return self._extras
+
+    @property
+    def actions(self) -> torch.Tensor:
+        """
+        The actions for each environment for this step.
+        If you're using an action manager, these are the actions prior to being handled by the action manager.
+        """
+        return self._actions
+
+    @property
+    def last_actions(self) -> torch.Tensor:
+        """
+        The actions for for the previous step.
+        """
+        return self._last_actions
+
+    @property
+    def num_actions(self) -> int:
+        """The number of actions for each environment."""
+        if self.action_space is not None:
+            return self.action_space.shape[0]
+        return 0
+
+    @property
+    def num_observations(self) -> int:
+        """The number of observations for each environment."""
+        if self.observation_space is not None:
+            return self.observation_space.shape[0]
+        return 0
+
+    @property
+    def max_episode_length_steps(self) -> int | None:
+        """
+        The max episode length, in steps, for each environment.
+        If episode randomization scaling is enabled, this will be the base max episode length before scaling.
+        """
+        return self._base_max_episode_length
+
+    """
+    Utilities
+    """
+
+    def set_max_episode_length(self, max_episode_length_sec: int) -> int:
+        """
+        Set or change the maximum episode length.
+
+        Args:
+            max_episode_length_sec: The maximum episode length in seconds.
+
+        Returns:
+            The maximum episode length in steps.
+        """
+        self._max_episode_length_sec = max_episode_length_sec
+        self._base_max_episode_length = math.ceil(max_episode_length_sec / self.dt)
+        return self._base_max_episode_length
+
+    """
+    Operations
+    """
+
+    def build(self) -> None:
+        """
+        Builds the environment before the first step.
+        The Genesis scene and all the scene entities must be added before calling this method.
+        """
+        assert (
+            self.scene is not None
+        ), "The scene must be constructed and assigned to the <env>.scene attribute before building."
+        self.scene.build(n_envs=self.num_envs)
+
+    def step(
+        self, actions: torch.Tensor
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, dict[str, Any]]:
+        """
+        Performs a step in all environments with the given actions.
+
+        Args:
+            actions: Batch of actions for each environment with the :attr:`action_space` shape.
+
+        Returns:
+            Batch of (observations, rewards, terminations, truncations, info/extras)
+        """
+        self._extras = {}
+        self._extras[self.extras_logging_key] = {}
+        self.step_count += 1
+        self.episode_length += 1
+
+        if self._actions is None:
+            self._actions = actions.detach().clone()
+            self._last_actions = torch.zeros_like(actions, device=self.device)
+        else:
+            self._last_actions[:] = self._actions[:]
+            self._actions[:] = actions[:]
+
+        return None, None, None, None, self._extras
+
+    def reset(
+        self,
+        envs_idx: list[int] = None,
+    ) -> tuple[torch.Tensor, dict[str, Any]]:
+        """
+        Reset one or more environments.
+        Each of the registered managers will also be reset for those environments.
+
+        Args:
+            env_ids: The environment ids to reset. If None, all environments are reset.
+
+        Returns:
+            A batch of observations and info from the vectorized environment.
+        """
+        if envs_idx is None:
+            envs_idx = torch.arange(self.num_envs, device=self.device)
+
+        # Initial reset, set buffers
+        if self.step_count == 0 and self.action_space is not None:
+            self._actions = torch.zeros(
+                (self.num_envs, self.action_space.shape[0]),
+                device=self.device,
+                dtype=torch.float,
+            )
+            self._last_actions = torch.zeros_like(self.actions, device=self.device)
+
+        # Actions
+        if envs_idx.numel() > 0:
+            if self.actions is not None:
+                self.actions[envs_idx] = 0.0
+                self._last_actions[envs_idx] = 0.0
+
+            # Episode length
+            self.episode_length[envs_idx] = 0
+
+        # Randomize max episode length for env_ids
+        if (
+            len(envs_idx) > 0
+            and self._max_episode_random_scaling > 0.0
+            and self._base_max_episode_length is not None
+        ):
+            max_random_scaling = (
+                self._base_max_episode_length * self._max_episode_random_scaling
+            )
+            randomization = (
+                torch.empty((envs_idx.numel(),)).uniform_(-1.0, 1.0)
+                * max_random_scaling
+            )
+            self.max_episode_length[envs_idx] = torch.round(
+                self._base_max_episode_length + randomization
+            ).to(torch.int)
+
+        return None, self.extras
+
+    def get_observations(self) -> torch.Tensor:
+        """
+        Returns the current observations for each environment.
+        Override this method to return the observations for your environment.
+
+        Example::
+
+            def get_observations(self) -> torch.Tensor:
+                return torch.cat(
+                [
+                    self.base_ang_vel * self.obs_scales["ang_vel"],  # 3
+                    self.projected_gravity,  # 3
+                    self.commands * self.commands_scale,  # 3
+                    (self.dof_pos - self.default_dof_pos) * self.obs_scales["dof_pos"],  # 12
+                    self.dof_vel * self.obs_scales["dof_vel"],  # 12
+                    self.actions,  # 12
+                ],
+                axis=-1,
+            )
+        """
+        if self.observation_space is not None:
+            return torch.zeros(
+                (self.num_envs, self.observation_space.shape[0]),
+                device=self.device,
+                dtype=torch.float,
+            )
+        return None
+
+    def close(self):
+        """Close the environment."""
+        pass
---
+++ genesis_forge/ros2_managed_env.py
@@ -0,0 +1,401 @@
+import torch
+from typing import Any, TypedDict
+from gymnasium import spaces
+from tensordict import TensorDict
+from genesis_forge.ros2_env import Ros2Env
+from genesis_forge.managers.base import BaseManager, ManagerType
+from genesis_forge.managers import (
+    ContactManager,
+    EntityManager,
+    CommandManager,
+    TerrainManager,
+    PositionActionManager,
+    ObservationManager,
+    RewardManager,
+    TerminationManager,
+    ActuatorManager,
+)
+
+
+class ManagersDict(TypedDict):
+    actuator: ActuatorManager | None
+    contact: list[ContactManager]
+    entity: list[EntityManager]
+    command: list[CommandManager]
+    terrain: list[TerrainManager]
+    action: PositionActionManager | None
+    observation: list[ObservationManager]
+    reward: RewardManager | None
+    termination: TerminationManager | None
+
+
+class Ros2ManagedEnvironment(Ros2Env):
+    """
+    An environment which moves a lot of the logic of the environment to manager classes.
+    This helps to keep the environment code clean and modular.
+
+    Args:
+        num_envs: Number of parallel environments.
+        dt: Simulation time step.
+        max_episode_length_sec: Maximum episode length in seconds.
+        max_episode_random_scaling: Randomly scale the maximum episode length by this amount (+/-) so that not all environments reset at the same time.
+        extras_logging_key: The key used, in info/extras dict, which is returned by step and reset functions, to send data to tensorboard by the RL agent.
+
+    Example::
+
+        class MyEnv(ManagedEnvironment):
+            def __init__(self, *args, **kwargs):
+                super().__init__(*args, **kwargs)
+
+                # ...Define scene here...
+
+            def config(self):
+                self.action_manager = PositionalActionManager(
+                    self,
+                    joint_names=".*",
+                    pd_kp=50,
+                    pd_kv=0.5,
+                    max_force=8.0,
+                    default_pos={
+                        # Hip joints
+                        "Leg[1-2]_Hip": -1.0,
+                        "Leg[3-4]_Hip": 1.0,
+                        # Femur joints
+                        "Leg[1-4]_Femur": 0.5,
+                        # Tibia joints
+                        "Leg[1-4]_Tibia": 0.6,
+                    },
+                )
+                self.reward_manager = RewardManager(
+                    self,
+                    term_cfg={
+                        "Default pose": {
+                            "weight": -1.0,
+                            "fn": rewards.dof_similar_to_default,
+                            "params": {
+                                "dof_action_manager": self.action_manager,
+                            },
+                        },
+                        "Base height": {
+                            "fn": mdp.rewards.base_height,
+                            "params": { "target_height": 0.135 },
+                            "weight": -100.0,
+                        },
+                    },
+                )
+                ObservationManager(
+                    self,
+                    cfg={
+                        "velocity_cmd": {"fn": self.velocity_command.observation},
+                        "robot_ang_vel": {
+                            "fn": utils.entity_ang_vel,
+                            "params": {"entity": self.robot},
+                            "noise": 0.1,
+                        },
+                        "robot_lin_vel": {
+                            "fn": utils.entity_lin_vel,
+                            "params": {"entity": self.robot},
+                            "noise": 0.1,
+                        },
+                        "robot_projected_gravity": {
+                            "fn": utils.entity_projected_gravity,
+                            "params": {"entity": self.robot},
+                            "noise": 0.1,
+                        },
+                        "robot_dofs_position": {
+                            "fn": self.action_manager.get_dofs_position,
+                            "noise": 0.01,
+                        },
+                        "actions": {"fn": lambda: env.actions},
+                    },
+                )
+    """
+
+    def __init__(
+        self,
+        num_envs: int = 1,
+        dt: float = 1 / 100,
+        max_episode_length_sec: int | None = 10,
+        max_episode_random_scaling: float = 0.0,
+        extras_logging_key: str = "episode",
+    ):
+        super().__init__(
+            num_envs=num_envs,
+            dt=dt,
+            max_episode_length_sec=max_episode_length_sec,
+            max_episode_random_scaling=max_episode_random_scaling,
+            extras_logging_key=extras_logging_key,
+        )
+        self.managers: ManagersDict = {
+            "contact": [],
+            "entity": [],
+            "command": [],
+            "terrain": [],
+            # there can only be one of each of these
+            "actuator": None,
+            "action": None,
+            "observation": [],
+            "reward": None,
+            "termination": None,
+        }
+
+        self._action_space = None
+        self._observation_space = None
+        self._reward_buf = torch.zeros(
+            (self.num_envs,), device=self.device, dtype=torch.float
+        )
+        self._terminated_buf = torch.zeros(
+            (self.num_envs,), device=self.device, dtype=torch.bool
+        )
+        self._truncated_buf = torch.zeros(
+            (self.num_envs,), device=self.device, dtype=torch.bool
+        )
+
+    """
+    Properties
+    """
+
+    @property
+    def action_space(self) -> torch.Tensor:
+        """
+        The action space, provided by the action manager, if it exists.
+        """
+        if self.managers["action"] is not None:
+            return self.managers["action"].action_space
+        if self._action_space is not None:
+            return self._action_space
+        return None
+
+    @action_space.setter
+    def action_space(self, action_space: spaces.Space):
+        """
+        Set the action space.
+        """
+        self._action_space = action_space
+
+    @property
+    def observation_space(self) -> spaces.Space:
+        """
+        The observation space for the "policy" observation manager, if it exists.
+        """
+        if len(self.managers["observation"]) > 0:
+            for obs in self.managers["observation"]:
+                if obs.name == "policy":
+                    return obs.observation_space
+            return self.managers["observation"][0].observation_space
+        if self._observation_space is not None:
+            return self._observation_space
+        return None
+
+    @observation_space.setter
+    def observation_space(self, observation_space: spaces.Space):
+        """
+        Set the observation space.
+        """
+        self._observation_space = observation_space
+
+    """
+    Managers
+    """
+
+    def add_manager(self, manager_type: ManagerType, manager: BaseManager):
+        """
+        Adds a manager to the environment.
+        This will automatically be called by the manager class.
+
+        Args:
+            manager_type: The type of manager to add.
+            manager: The manager to add.
+        """
+        if manager_type not in self.managers:
+            raise ValueError(f"'{manager_type}' is not a valid manager type.")
+
+        # Append manager if the dict item is a list
+        if isinstance(self.managers[manager_type], list):
+            self.managers[manager_type].append(manager)
+        elif self.managers[manager_type] is None:
+            self.managers[manager_type] = manager
+        else:
+            raise ValueError(
+                f"Manager type '{manager_type}' already has a manager, and an environment cannot have multiple {manager_type} managers."
+            )
+
+    """
+    Operations
+    """
+
+    def config(self):
+        """
+        Override this method and initialize all your managers here.
+
+        Example::
+
+            def config(self):
+                EntityManager(
+                    self,
+                    entity_attr="robot",
+                    on_reset={
+                        "position": {
+                            "fn": reset.position,
+                            "params": {
+                                "position": INITIAL_BODY_POSITION,
+                                "quat": INITIAL_QUAT,
+                            },
+                        },
+                    },
+                )
+        """
+        pass
+
+    def build(self):
+        """
+        Builds the environment before the first step.
+        The Genesis scene and all the scene entities must be added before calling this method.
+        """
+        super().build()
+        self.config()
+
+        for terrain_manager in self.managers["terrain"]:
+            terrain_manager.build()
+        if self.managers["actuator"] is not None:
+            self.managers["actuator"].build()
+        if self.managers["action"] is not None:
+            self.managers["action"].build()
+        for contact_manager in self.managers["contact"]:
+            contact_manager.build()
+        if self.managers["termination"] is not None:
+            self.managers["termination"].build()
+        if self.managers["reward"] is not None:
+            self.managers["reward"].build()
+        for command_manager in self.managers["command"]:
+            command_manager.build()
+        for entity_manager in self.managers["entity"]:
+            entity_manager.build()
+        for obs in self.managers["observation"]:
+            obs.build()
+
+    def step(
+        self, actions: torch.Tensor,
+    ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, dict[str, Any]]:
+        """
+        Performs a step in all environments with the given actions.
+
+        Args:
+            actions: Batch of actions for each environment with the :attr:`action_space` shape.
+
+        Returns:
+            Batch of (observations, rewards, terminations, truncations, extras)
+        """
+        super().step(actions)
+        self.extras["observations"] = TensorDict({}, device=self.device)
+
+        # Execute the actions and a simulation step
+        if self.managers["action"] is not None:
+            self.managers["action"].step(actions)
+
+        # Update entity managers
+        for entity_manager in self.managers["entity"]:
+            entity_manager.step()
+
+        # Calculate contact forces
+        for contact_manager in self.managers["contact"]:
+            contact_manager.step()
+
+        # Calculate termination and truncation
+        reset_env_idx = None
+        truncated = self._truncated_buf
+        terminated = self._terminated_buf
+        if self.managers["termination"] is not None:
+            terminated, truncated = self.managers["termination"].step()
+            reset_env_idx = (
+                (terminated | truncated).nonzero(as_tuple=False).reshape((-1,)).detach()
+            )
+
+        # Calculate rewards
+        rewards = self._reward_buf
+        if self.managers["reward"] is not None:
+            rewards = self.managers["reward"].step()
+
+        # Command managers
+        for command_manager in self.managers["command"]:
+            command_manager.step()
+
+        # Reset environments
+        if reset_env_idx is not None and reset_env_idx.numel() > 0:
+            self.reset(reset_env_idx)
+
+        # Get observations
+        obs = self.get_observations()
+        return (
+            obs,
+            rewards,
+            terminated,
+            truncated,
+            self.extras,
+        )
+
+    def reset(
+        self, env_ids: list[int] | None = None
+    ) -> tuple[torch.Tensor, dict[str, Any]]:
+        """
+        Reset one or more environments.
+        Each of the registered managers will also be reset for those environments.
+
+        Args:
+            env_ids: The environment ids to reset. If None, all environments are reset.
+
+        Returns:
+            A batch of observations (if env_ids is None) and an info dictionary from the vectorized environment.
+        """
+        (obs, _) = super().reset(env_ids)
+
+        if self.managers["actuator"] is not None:
+            self.managers["actuator"].reset(env_ids)
+        if self.managers["action"] is not None:
+            self.managers["action"].reset(env_ids)
+        for entity_manager in self.managers["entity"]:
+            entity_manager.reset(env_ids)
+        for contact_manager in self.managers["contact"]:
+            contact_manager.reset(env_ids)
+        if self.managers["termination"] is not None:
+            self.managers["termination"].reset(env_ids)
+        if self.managers["reward"] is not None:
+            self.managers["reward"].reset(env_ids)
+        for command_manager in self.managers["command"]:
+            command_manager.reset(env_ids)
+        for obs_manager in self.managers["observation"]:
+            obs_manager.reset(env_ids)
+
+        # Only get observations when env_ids is None because this will be the initial reset called before the first step
+        # Otherwise, the observations are ignored
+        if env_ids is None:
+            obs = self.get_observations()
+
+        return obs, self.extras
+
+    def get_observations(self) -> torch.Tensor:
+        """
+        Returns the current observations for this step.
+        If you use the ObservationManager, this will be handled automatically.
+        Otherwise, override this method to return the observations.
+        """
+        if len(self.managers["observation"]) > 0:
+            # We already have observations for this step
+            if (
+                "observations" in self.extras
+                and "policy" in self.extras["observations"]
+            ):
+                return self.extras["observations"]["policy"]
+            if "observations" not in self.extras:
+                self.extras["observations"] = TensorDict({}, device=gs.device)
+
+            # Get observations
+            policy_obs = None
+            for obs_manager in self.managers["observation"]:
+                obs = obs_manager.get_observations()
+                self.extras["observations"][obs_manager.name] = obs
+                if obs_manager.name == "policy":
+                    policy_obs = obs
+            return policy_obs
+
+        return super().get_observations()
